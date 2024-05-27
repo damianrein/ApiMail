222c4ba8578ebee12310bf261339ff3d828af475
damianrein <damianrein464@gmail.com>
2024-05-27 19:24:55 -0300

    -

--- src/main/java/com/ApiMail/controller/MailController.java
+++ src/main/java/com/ApiMail/controller/MailController.java
@@ -1,11 +1,31 @@
 package com.ApiMail.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ApiMail.model.MailSimple;
+import com.ApiMail.service.MailService;
 
 @RestController
 @RequestMapping("/email")
 public class MailController {
 
+	private MailService mailService;
 	
+	@PostMapping
+	public ResponseEntity<?> requestEmail(@RequestBody MailSimple request){
+		
+		mailService.sendEmail(request.getToUser(), request.getSubject(), request.getMessage());
+		
+		Map<String, String> response = new HashMap<>();
+		response.put("estado", "Enviado");
+		
+		return ResponseEntity.ok(response);
+	}
 }
