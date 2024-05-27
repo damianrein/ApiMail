c4deb987536cc36b875923fc9f5523442d7a5b1c
damianrein <damianrein464@gmail.com>
2024-05-26 23:12:57 -0300

    -

--- src/main/java/com/ApiMail/config/MailConfig.java
+++ src/main/java/com/ApiMail/config/MailConfig.java
@@ -1,5 +1,38 @@
 package com.ApiMail.config;
 
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
 public class MailConfig {
 
+	@Value("${email.sender}")
+	private String userEmail;
+	
+	@Value("${email.password}")
+	private String password;
+	
+	@Bean
+	JavaMailSender getJavaMailSender() {
+		
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
+		mailSender.setUsername(userEmail);
+		mailSender.setPassword(password);
+		
+		Properties prop = mailSender.getJavaMailProperties();
+		prop.put("mail.transport.protocol", "smtp");
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.starttls.enable", "true");
+		prop.put("mail.debug", "true");
+		
+		return mailSender;
+	}
 }
--- src/main/java/com/ApiMail/controller/MailController.java
+++ src/main/java/com/ApiMail/controller/MailController.java
@@ -1,5 +1,11 @@
 package com.ApiMail.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/email")
 public class MailController {
 
+	
 }
