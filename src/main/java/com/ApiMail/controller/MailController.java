dd449f9a92181402abc362e7773db6960c686dda
damianrein <damianrein464@gmail.com>
2024-05-27 19:57:22 -0300

    -

--- src/main/java/com/ApiMail/controller/MailController.java
+++ src/main/java/com/ApiMail/controller/MailController.java
@@ -1,5 +1,11 @@
 package com.ApiMail.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ApiMail.model.MailFile;
 import com.ApiMail.model.MailSimple;
 import com.ApiMail.service.MailService;
 
@@ -18,13 +25,38 @@ public class MailController {
 
 	private MailService mailService;
 	
-	@PostMapping
+	public MailController(MailService mailService) {
+		this.mailService = mailService;
+	}
+
+	@PostMapping("/email")
 	public ResponseEntity<?> requestEmail(@RequestBody MailSimple request){
 		
 		mailService.sendEmail(request.getToUser(), request.getSubject(), request.getMessage());
 		
 		Map<String, String> response = new HashMap<>();
 		response.put("estado", "Enviado");
+		
+		return ResponseEntity.ok(response);
+	}
+	
+	@PostMapping("/emailFile")
+	public ResponseEntity<?> requestEmailWithFile(@RequestBody MailFile request) throws IOException{
+		
+		String filename = request.getFile().getOriginalFilename();
+		
+		Path path = Paths.get("src/main/resources/files" + filename);
+		
+		Files.createDirectories(path.getParent());
+		Files.copy(request.getFile().getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+		
+		File file = path.toFile();
+		
+		mailService.sendEmail(request.getToUser(), request.getSubject(), request.getMessage(), file);
+		
+		Map<String, String> response = new HashMap<>();
+		response.put("estado", "Enviado");
+		response.put("archivo", filename);
 		
 		return ResponseEntity.ok(response);
 	}
